a700af0d4137c735f18cedac54c6b8d77dc18dba
Seijno <seijnovannieuwkoop@gmail.com>
2023-12-19 14:25:46 +0100

    content filled

--- scripts/sidemenu.js
+++ scripts/sidemenu.js
@@ -4,8 +4,10 @@ document.getElementById("closeMenu").addEventListener("click", closeMenu);
 
 function openMenu() {
 document.getElementById("sideMenu").style.width = "50vw";
+// document.getElementById("openMenu").style.display = "none";
 }
 
 function closeMenu() {
 document.getElementById("sideMenu").style.width = "0";
+// document.getElementById("openMenu").style.display = "block";
 }
